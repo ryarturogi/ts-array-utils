625f67881056c5557cd6434fc343400643cbd6c8
Ricardo Guillen I <ricardo.guillen@elaniin.com>
2023-01-04 12:06:26 -0400

    Add tests

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,50 @@
 export function sort(array: number[]): number[] {
-  return array.sort((a, b) => a - b);
+  return array.sort((a, b) => a - b)
 }
 
-export function filter(array: number[], predicate: (n: number) => boolean): number[] {
-  return array.filter(predicate);
+export function filter(
+  array: number[],
+  predicate: (n: number) => boolean
+): number[] {
+  return array.filter(predicate)
 }
 
-export function transform(array: number[], transformer: (n: number) => number): number[] {
-  return array.map(transformer);
+export function transform(
+  array: number[],
+  transformer: (n: number) => number
+): number[] {
+  return array.map(transformer)
 }
 
 export function includes(array: number[], element: number): boolean {
-  return array.indexOf(element) !== -1;
+  return array.indexOf(element) !== -1
 }
 
 export function min(array: number[]): number | undefined {
-  return array.length ? Math.min(...array) : undefined;
+  return array.length ? Math.min(...array) : undefined
 }
 
 export function max(array: number[]): number | undefined {
-  return array.length ? Math.max(...array) : undefined;
+  return array.length ? Math.max(...array) : undefined
 }
 
 export function sum(array: number[]): number {
-  return array.reduce((acc, cur) => acc + cur, 0);
+  return array.reduce((acc, cur) => acc + cur, 0)
 }
 
 export function average(array: number[]): number {
-  return array.length ? sum(array) / array.length : 0;
+  return array.length ? sum(array) / array.length : 0
 }
 
 export function distinct(array: number[]): number[] {
-  return Array.from(new Set(array));
+  return Array.from(new Set(array))
 }
 
 export function shuffle(array: number[]): number[] {
-  const shuffled = [...array];
+  const shuffled = [...array]
   for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
   }
-  return shuffled;
+  return shuffled
 }
